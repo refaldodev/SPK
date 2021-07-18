eabe2425596cfaddea50e672454ecd195247bfbc
refaldodev <m.refaldorachman@gmail.com>
2021-07-19 00:02:19 +0700

    delete composer library myth/auth

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -33,7 +33,7 @@ class Filters extends BaseConfig
 	public $globals = [
 		'before' => [
 			'honeypot',
-			'login'
+			// 'login'
 			// 'csrf',
 		],
 		'after'  => [
@@ -62,5 +62,7 @@ class Filters extends BaseConfig
 	 *
 	 * @var array
 	 */
-	public $filters = [];
+	public $filters = [
+		'login' => ['before' => ['datadosen/*', 'datadosen']],
+	];
 }
